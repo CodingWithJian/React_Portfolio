9e6d4c6bc10bb9b3f0880adbaf82b753bec169b9
Jian Qi <jianqi1997@gmail.com>
2020-05-29 19:36:59 -0700

    Edit information

--- reactportfolio/src/Components/Header.js
+++ reactportfolio/src/Components/Header.js
@@ -7,6 +7,11 @@ class Header extends Component {
       var name = this.props.data.name;
       var occupation= this.props.data.occupation;
       var description= this.props.data.description;
+
+      var networks= this.props.data.social.map(function(network){
+         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+       })
+
     }
 
     return (
@@ -30,6 +35,12 @@ class Header extends Component {
          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
             <h3>I'm San Francisco based Front-End Software Engineer. {description}.</h3>
+            
+            <hr />
+            <ul className="social">
+               {networks}
+            </ul>
+
          </div>
       </div>
 
